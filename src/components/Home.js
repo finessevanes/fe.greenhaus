bb01d67cf8610bc136b7061dedf06b7af7e7908a
Vanessa Mercado <vanessa.mercado24@gmail.com>
2019-10-18 15:54:21 -0700

    added material uui date

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home=()=>{
   return(
     <div>
-      This is my home page
+      This is my home padfvsdfge
     </div>
   )
 }
--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+
 
 const TipForm=(props)=>{
   const [id, setId] = React.useState(0);
-  const [date, setDate] = React.useState("");
+  const [date, setDate] = React.useState(new Date('2014-08-18T21:11:54'));
   const [tipAmount, setTipAmount] = React.useState("");
   const [savingsRate, setSavingsRate] = React.useState("");
   const [amountSaved, setAmountSaved] = React.useState("");
@@ -10,8 +20,8 @@ const TipForm=(props)=>{
   const onIdInput=(e)=>{
     setId(e.target.value);
   }
-  const onDateInput=(e)=>{
-    setDate(e.target.value);
+  const onDateInput=(date)=>{
+    setDate(date);
   }
   const onTipAmountInput=(e)=>{
     setTipAmount(e.target.value);
@@ -80,7 +90,23 @@ const TipForm=(props)=>{
   return(
     <div>
       <h1>This is my formpage page</h1>
-      <input type="date" value={date} onChange={onDateInput}></input>
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog"
+            label="Date picker dialog"
+            format="MM/dd/yyyy"
+            value={date}
+            onChange={onDateInput}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          </Grid>
+      </MuiPickersUtilsProvider>
+
       <input type="number" value={tipAmount} onChange={onTipAmountInput} placeholder="Tip Amount"></input>
       <select type="number" value={savingsRate} onChange={onSavingsRateInput}>
         <option>Savings Rate%</option>
