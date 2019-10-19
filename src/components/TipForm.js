ec9ce50a289166738bba1f64d73f9773f6ded60e
Vanessa Mercado <vanessa.mercado24@gmail.com>
2019-10-18 17:00:45 -0700

    material ui form

--- src/components/TipForm.js
+++ src/components/TipForm.js
@@ -7,12 +7,47 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 
 const TipForm=(props)=>{
+  const ranges = [
+    {
+      value: '.15',
+      label: '15%',
+    },
+    {
+      value: '.2',
+      label: '20%',
+    },
+    {
+      value: '.25',
+      label: '25%',
+    },
+  ];
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    textField: {
+      flexBasis: 200,
+    },
+  }));
+
+  const classes = useStyles();
+
   const [id, setId] = React.useState(0);
-  const [date, setDate] = React.useState(new Date('2014-08-18T21:11:54'));
+  const [date, setDate] = React.useState(new Date());
   const [tipAmount, setTipAmount] = React.useState("");
   const [savingsRate, setSavingsRate] = React.useState("");
   const [amountSaved, setAmountSaved] = React.useState("");
@@ -89,37 +124,79 @@ const TipForm=(props)=>{
   }
   return(
     <div>
-      <h1>This is my formpage page</h1>
+      <div className={classes.root}>
+                <Grid container justify="space-around">
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
 
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Grid container justify="space-around">
-          <KeyboardDatePicker
-            margin="normal"
-            id="date-picker-dialog"
-            label="Date picker dialog"
-            format="MM/dd/yyyy"
-            value={date}
-            onChange={onDateInput}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
+            <KeyboardDatePicker
+
+              margin="normal"
+              id="date-picker-dialog"
+              label="Date picker dialog"
+              format="MM/dd/yyyy"
+              value={date}
+              onChange={onDateInput}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+
+        </MuiPickersUtilsProvider>
+
+        <TextField
+          id="outlined-adornment-amount"
+          className={clsx(classes.margin, classes.textField)}
+          variant="outlined"
+          label="Tip Amount"
+          value={tipAmount}
+          onChange={onTipAmountInput}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+
+        <TextField
+          select
+          className={clsx(classes.margin, classes.textField)}
+          variant="outlined"
+          label="Savings Rate"
+          value={savingsRate}
+          onChange={onSavingsRateInput}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">%</InputAdornment>,
+          }}
+        >
+          {ranges.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+
+        </TextField>
+
+        <TextField
+          id="outlined-adornment-amount"
+          className={clsx(classes.margin, classes.textField)}
+          variant="outlined"
+          label="Amount to Save"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">${Math.ceil(tipAmount*savingsRate)}</InputAdornment>,
+          }}
+        />
+        <TextField
+          id="outlined-adornment-amount"
+          className={clsx(classes.margin, classes.textField)}
+          variant="outlined"
+          label="Actual Amount Saved"
+          value={amountSaved}
+          onChange={onAmountSavedInput}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
           </Grid>
-      </MuiPickersUtilsProvider>
-
-      <input type="number" value={tipAmount} onChange={onTipAmountInput} placeholder="Tip Amount"></input>
-      <select type="number" value={savingsRate} onChange={onSavingsRateInput}>
-        <option>Savings Rate%</option>
-        <option value=".15">15%</option>
-        <option value=".20">20%</option>
-        <option value=".25">25%</option>
-      </select>
-      <div>
-        <p>Amount to save: ${Math.ceil(tipAmount*savingsRate)}</p>
-        <input type="number" placeholder="Actual Saved" value={amountSaved} onChange={onAmountSavedInput}></input>
-      </div>
+        </div>
       {buttonAction}
     </div>
-  )
-}
+  )}
 export default TipForm
